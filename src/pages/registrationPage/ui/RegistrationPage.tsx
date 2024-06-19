32baddc58c53ba5eeba5d29b76bb88b4b810c521
Ekaterina Shakirova <ekaterinashakirovaa@mail.ru>
2024-06-19 23:10:40 +0500

    chore: start of background for registration page

--- src/pages/registrationPage/ui/RegistrationPage.tsx
+++ src/pages/registrationPage/ui/RegistrationPage.tsx
@@ -5,7 +5,14 @@ import { StarterForm } from "../../../widgets/starterForm";
 export const RegistrationPage = () => {
     return (
         <>
-        <StarterForm headerText='Регистрация' amount={3} logo1='vk' logo2='yandex' logo3='google'/>
+            <div className='background'>
+                <img src="src\shared\assets\images\staticCircle1.svg" />
+                <img src="src\shared\assets\images\staticCircle2.svg"/>
+                <img src="src\shared\assets\images\staticCircle3.svg"/>
+                <img src="src\shared\assets\images\textBG.svg"/>
+            </div>
+            <StarterForm headerText='Регистрация' amount={3} logo1='vk' logo2='yandex' logo3='google' />
         </>
+
     )
 }
